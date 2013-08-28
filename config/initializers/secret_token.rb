30680f8d8c2f0f5aac5f497a0a9d8d2f4e103a24
Chris Taylor <cctaylor@gmail.com>
2013-08-28 08:06:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CCV4::Application.config.secret_token = '88ffdf4367f49a629adc2af69a08c16e9031b64a0d297935d57a10d7a31b6a89c03c4ecf06dc51b7765b89295f3051dbb19a277a839cb4d4ef01e5972e83e5ff'
